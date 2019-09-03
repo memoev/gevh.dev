ceca1f45eb1a7620c7d3f8cc2329755c86b72abf
Guillermo Villalta <mexcelus@gmail.com>
2019-09-02 20:14:07 -0600

    added projects button to render projects and go back to main container

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -7,3 +7,13 @@ $(".go-back").click(function () {
     $(".hidden").fadeOut(500);
     $(".main").fadeTo('slow', 1);
 })
+
+$(".projects").click(function () {
+    $(".main").fadeOut(500);
+    $(".hidden-2").fadeIn(1500);
+})
+
+$(".go-back-2").click(function () {
+    $(".hidden-2").fadeOut(500);
+    $(".main").fadeTo('slow', 1);
+})
